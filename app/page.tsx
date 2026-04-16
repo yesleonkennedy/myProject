117295fbec741c1bcc5314f1d9a380638e1f2bf7
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2026-04-17 01:51:37 +0530

    added colors as suggested by the maintainer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="text-center">
+    <div className="text-center text-cyan-300 text-2xl font-bold">
       test
       loading feature implemented
     </div>
